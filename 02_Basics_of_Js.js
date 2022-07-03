6887aeb2538d3f457a980b54b6e7b768d5f4bf0d
Rushi-Varmora <rushi.varmora107922@marwadiuniversity.ac.in>
2022-07-03 12:58:58 +0530

    updated

---
+++ 02_Basics_of_Js.js
@@ -0,0 +1,103 @@
+/*
+
+let x,y,z ;
+var p ;
+const q = 10 ;
+
+let xyz = 'xyz';
+xyz = 'pqr';
+
+x = 5;
+y = 10;
+z = x + y;
+
+console.log('X:',x,'Y:',y,'Z:',z);
+
+
+//Difference between == and ===
+
+let a = 2;
+let b = '2';
+console.log('\na == b',a==b); //true
+console.log('\na === b',a===b); //false
+
+
+
+let w =2;
+let v = 3;
+
+console.log('v+w',v+w); //addition
+console.log('v-w',v-w); //Subtraction
+console.log('v*w',v*w); //Multiplication
+console.log('v**w',v**w); //exponentiation
+console.log('v/w',v/w); //Division
+console.log('v%w',v%w); //Modulus
+console.log('++v',++v); //Increment
+console.log('--w',--w); //Decrement
+
+
+
+let arr = [1,2,3,4,5];
+console.log(arr);
+
+*/
+
+// This is how we do comment in Javascript file.
+
+// Type of Data
+let m = 3;      //number
+let n = 'abc';  //string
+let o = {
+    id: 'abc123',
+    name: 'abc',
+    age: '20'
+};    
+
+
+let x, y, z; // Variable declaration
+var p;
+
+const s = 20;
+
+let xyz = "xyz";
+xyz = "pqr";
+
+x = 5; // Variable assignment
+y = 6;
+z = x + y; // Arithmatic operation
+
+console.log("x:", x, "y:", y, "z:", z); // Printing in terminal
+
+
+// Arithmatic Operators
+let w = 2;
+let v = 3;
+console.log("v+w", v + w); //Addition
+console.log("v-w", v - w); //Sub
+console.log("v*w", v * w); //Multiply
+console.log("v**w", v ** w); //Expo
+console.log("v/w", v / w); //Division    
+console.log("v%w", v % w); //Mod        
+console.log("++v", ++v); //Increment
+console.log("--w", --w); //Decrement
+
+
+
+// Difference between == and ===
+let a = 3;
+let b = 3;
+
+console.log("\na==b : ", a == b); // Compares just value
+console.log("a===b : ", a === b); // Compares values and type
+
+
+
+// string methods
+let str1 = 'Hello Everyone';
+console.log("string lenth :", str1.length);
+
+//slice (startIndex,endIndex)
+console.log(str1.slice(6,9));
+console.log(str1.slice(2,4));
+
+//Substring (startIndex,endIndex)
